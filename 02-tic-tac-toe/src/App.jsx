30298d3628af8216dc1e60e5066e9da5342be0ea
Fernando Ramos <fndo.ramos@gmail.com>
2023-11-17 14:17:53 +0100

    add game logic for winner

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react"
 
 const TURNS = {
@@ -5,10 +6,26 @@ const TURNS = {
   O: 'o'
 }
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
-const Square = ({children, updateBoard, index}) => {
+
+const Square = ({children, isSelected, updateBoard, index}) => {
+  const className =  `square ${isSelected ? 'is-selected' : ''}` 
+
+  const handleOnClick = () => {
+    updateBoard(index)
+  }
   return (
-    <div className="square">
+    <div className={className} onClick={handleOnClick}>
       {children}
     </div>
   )
@@ -16,6 +33,45 @@ const Square = ({children, updateBoard, index}) => {
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null) //null = no hay ganador, false = hay un empate
+
+  const checkWinner = (boardToCheck) => {
+    //revisamos todas las combinaciones ganadoras
+    //para ver si X o O ganó
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] && 
+        boardToCheck[a] === boardToCheck[b] && 
+        boardToCheck[a] === boardToCheck[c]
+        ) {
+          return boardToCheck[a]
+        }
+      }
+      //si no hay ganador
+      return null
+  }
+
+  const updateBoard = (index) => {
+    //no actualizamos este square si ya tiene algo
+    if(board[index] || winner) return
+
+    //actualizamos el tlablero
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    //actualizamos turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+
+    //revisar si hay ganador
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    }
+  }
 
   return (
     <main className="board">
@@ -27,12 +83,19 @@ function App() {
               <Square
                 key={index}
                 index={index}
+                updateBoard={updateBoard}
+                
               >
                 {board[index]}
               </Square>
             )
           })
         }
+      </section>
+
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
     </main>
   )
