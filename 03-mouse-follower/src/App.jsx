d65c85c10ff1f2f93afbc54e6eaa51ba28b9cc71
Fernando Ramos <fndo.ramos@gmail.com>
2023-11-24 14:08:34 +0100

    Add pointer follower and styles

--- 03-mouse-follower/src/App.jsx
+++ 03-mouse-follower/src/App.jsx
@@ -1,5 +1,48 @@
+import { useState, useEffect } from "react";
+
 function App() {
-  return <h3>Project 3</h3>;
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event;
+      setPosition({ x: clientX, y: clientY });
+    };
+
+    if (enabled) {
+      window.addEventListener("pointermove", handleMove);
+    }
+
+    //cleanup:
+    //--> cuando el componente se desmonta
+    //--> cuando cambian las dependencias, antes de ejecutar
+    //    el efecto de nuevo
+    return () => {
+      window.removeEventListener("pointermove", handleMove);
+    };
+  }, [enabled]);
+  return (
+    <main>
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "#09F",
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          top: -20,
+          left: -20,
+          width: "40px",
+          height: "40px",
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? "Desactivar" : "Activar"} seguir puntero
+      </button>
+    </main>
+  );
 }
 
 export default App;
