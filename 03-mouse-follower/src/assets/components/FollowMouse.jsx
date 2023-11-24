b2b645971301cdbff6927148f6a54a9912a01595
Fernando Ramos <fndo.ramos@gmail.com>
2023-11-24 14:42:28 +0100

    Extract to components

---
+++ 03-mouse-follower/src/assets/components/FollowMouse.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+
+export function FollowMouse() {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event;
+      setPosition({ x: clientX, y: clientY });
+    };
+
+    if (enabled) {
+      window.addEventListener("pointermove", handleMove);
+    }
+
+    //cleanup:
+    //--> cuando el componente se desmonta
+    //--> cuando cambian las dependencias, antes de ejecutar
+    //    el efecto de nuevo
+    return () => {
+      window.removeEventListener("pointermove", handleMove);
+    };
+  }, [enabled]);
+  return (
+    <>
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "#09F",
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          top: -20,
+          left: -20,
+          width: "40px",
+          height: "40px",
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      />
+
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? "Desactivar" : "Activar"} seguir puntero
+      </button>
+    </>
+  );
+}
